515fac7268eb99ecb2c8a0d349c1379d636ee531
Mathieu Schroeter <mathieu@schroetersa.ch>
2026-05-13 14:03:24 +0200

    Rename rtAudioBackend to audioBackend

--- config.js
+++ config.js
@@ -57,7 +57,7 @@ module.exports = [
   },
   {
     type: 'input',
-    name: 'rtAudioBackend',
+    name: 'audioBackend',
     message:
       'leave empty for unspecified, or choose between: core, alsa, pulse, wasapi, ds',
     default: '',
